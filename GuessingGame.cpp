2fb85fef6bf6f771bbd4ad6f0e1d2c0ec72df5cf
RyanL20 <ryannhatkhoa@gmail.com>
2020-09-24 19:43:15 +0100

    Testing2

---
+++ GuessingGame.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+// boolean stillPlaying == true;
+
+ // while (stillPlaying == true) {
+    srand(time(NULL));
+    int rnadomNum = 0;
+    int randomNum = rand() * 100;
+    cout << randomNum;
+  
+
+
+}
+
+
+
+
+
+  
