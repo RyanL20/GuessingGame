5e2f3cd7f95e40635fc6aa945979ef800b1b2512
RyanL20 <ryannhatkhoa@gmail.com>
2020-09-25 03:15:13 +0100

    Has the random number generator, and if guess is too high or low.

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
 
 int main() {
-// boolean stillPlaying == true;
 
- // while (stillPlaying == true) {
-    srand(time(NULL));
-    int rnadomNum = 0;
-    int randomNum = rand() * 100;
-    cout << randomNum;
-//Testing  
+  int input;
+  cout << "Enter a number between 0-100" << endl;
+  cin >> input;
+  cout << "You entered: " << input << endl;  
 
+  //Need to check user's input
 
+  
+  //generate random number
+  //help from  
+
+  int rand_num = 0;
+   srand(unsigned(time(NULL)));
+   rand_num = rand()%101;
+
+   cout << "RandomNumber:" << rand_num << endl;
+
+   //Compare the number to random number
+   while (input != rand_num) {
+     if (input < rand_num) {
+       cout << "Guess Higher: ";
+       cin >> input;
+     }
+     else if (input > rand_num) {
+       cout << "Guess Lower: ";
+       cin >> input;
+     }
+   }
+   return 0;
 }
 
 
